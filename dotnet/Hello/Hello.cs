bc76c9b496e5760ef1ededbf2aa7da38c06bd92a
Toshihiko Asahi <lovingcat222@gmail.com>
2021-04-04 22:48:51 +0900

    dotnet new --output プロジェクト名でプロジェクト作る(プロジェクト作らないと動かない)
    dotnet run --project プロジェクト名でビルド・実行
    対象ファイルは特に指定しなくても自動的に読み込まれる
    
    dotnet new gitignoreで.gitignoreを自動生成

---
+++ dotnet/Hello/Hello.cs
@@ -0,0 +1,17 @@
+using System;
+
+// .NET FrameworkのコンパイラならC:/Windows/Microsoft.NET/Framework64/[バージョン]/にパスを通すだけ
+// .NET Core使うなら→https://docs.microsoft.com/ja-jp/dotnet/core/get-started
+//  dotnet new --output プロジェクト名
+//  dotnet run --project プロジェクト名
+
+namespace hoge
+{
+    public class Hello
+    {
+        static void Main()
+        {
+        	Console.WriteLine("OK");
+        }
+    }
+}
