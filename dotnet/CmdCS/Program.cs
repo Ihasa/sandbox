82e481e4011f636e0ca26f87785b46bc17cf8bfb
Toshihiko Asahi <lovingcat222@gmail.com>
2021-04-06 00:19:46 +0900

    grep的関数をC#で実装

---
+++ dotnet/CmdCS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CmdCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            csGrep("\\[17:", "../grepstudy/latest.log", "n");
+        }
+
+        static List<string> findList;
+        static void csGrep(string pat, string filePath,  string option){
+            findList = new List<string>();
+            using(StreamReader sr = new StreamReader(filePath)){
+                int rowNum = 1;
+                while(!sr.EndOfStream){
+                    string s = sr.ReadLine();
+                    Regex exp = new Regex(pat);
+                    if(exp.IsMatch(s)){
+                        string res = (option == "n") ? rowNum + ":" + s : s;
+                        findList.Add(res);
+                    }
+                    rowNum++;
+                }
+            }
+
+            if(option == "c"){
+                Console.WriteLine(findList.Count);
+            } else {
+                foreach(string s in findList){
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
