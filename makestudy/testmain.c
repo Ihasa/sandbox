853b8200492b3c38aba268936ae813c1ff117a2e
Toshihiko Asahi <lovingcat222@gmail.com>
2021-05-30 19:23:06 +0900

    make statictestでstaticな関数もテストする (同名static関数があるとエラー)

--- makestudy/testmain.c
+++ makestudy/testmain.c
@@ -6,6 +6,14 @@ void testSum(){
     CU_ASSERT_EQUAL(sum(-1, 3), 2);
 }
 
+#ifdef static
+extern float sumF(float[],int);
+void testSumF(){
+    float data[] = {3.0f,2.0f,1.0f,10.0f};
+    CU_ASSERT_DOUBLE_EQUAL(sumF(data,4),16.0f,0.0001f);
+}
+#endif
+
 void testAverage(){
     float data[] = {3.0f,2.0f,1.0f,10.0f};
     CU_ASSERT_DOUBLE_EQUAL(average(data,4),4.0f,0.0001f);
@@ -17,6 +25,9 @@ int main(){
     CU_pSuite suite = CU_add_suite("testtest",NULL,NULL);
     CU_add_test(suite,"test1",testSum);
     CU_add_test(suite,"testAverage", testAverage);
+#ifdef static
+    CU_add_test(suite,"test2", testSumF);
+#endif
     CU_console_run_tests();
     CU_cleanup_registry();
 }
